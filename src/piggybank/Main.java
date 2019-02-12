19a2cd9d4153abf097d6b9619b3aa65985eb4509
johnoro <officialjohnorourke@gmail.com>
2019-02-12 16:36:43 -0500

    Revert "Add rough implementation of Main for piggybank"
    This reverts commit 6cdb8b2d251f7bf85ae35b8824f3abf9db86c1c4.

--- src/piggybank/Main.java
+++ src/piggybank/Main.java
@@ -2,16 +2,6 @@ package piggybank;
 
 public class Main {
   public static void main(String[] args) {
-    PiggyBank piggyBank = new PiggyBank();
-    
-    piggyBank.add(new Quarter());
-    piggyBank.add(new Dime());
-    piggyBank.add(new Dollar(5));
-    piggyBank.add(new Nickle(3));
-    piggyBank.add(new Dime(7));
-    piggyBank.add(new Dollar());
-    piggyBank.add(new Penny(10));
-
-    System.out.println(piggyBank.getMoney());
+    System.out.println("It lives!");
   }
 }
